fdb3137d1e2dbe5ea7e3bc0a699a63d4802b1a08
Dasha <borovskaydarya14@gmail.com>
2024-05-06 18:01:07 +0000

    first

--- index.php
+++ index.php
@@ -1,27 +1,75 @@
 <?php
+// Загрузка данных из файла CSV
+$data = array_map('str_getcsv', file('titanic.csv'));
+$columns = array_shift($data);
+$passengers = array();
+foreach ($data as $row) {
+    $passengers[] = array_combine($columns, $row);
+}
 
-if (isset($_GET["NUM"])) {
-    
-    $num = (int)$_GET["NUM"];
-    IF ($NUM < 1000)
-    {
-        for ($i = 2; $i <= $num; $i++){
-            $is_prime = true;
-            for ($j=2; $j < $i; $j++){
-                if ($i % $j === 0){
-                    $is_prime = false;
-                    break;
-                }
-            }
-            if ($is_prime){
-                echo $i. " ";
-            }
+// Поиск пассажиров по возрасту
+function searchByAge($passengers, $age) {
+    $results = array();
+    foreach ($passengers as $passenger) {
+        if ($passenger['Age'] == $age) {
+            $results[] = $passenger;
         }
     }
-} elseif (isset($_GET['name'])) {
-    $name = htmlspecialchars($_GET["name"]);
-    echo 'Привет, '. $name. '!';
+    return $results;
+}
+
+// Поиск пассажиров по имени с использованием регулярного выражения
+function searchByName($passengers, $name) {
+    $results = array();
+    foreach ($passengers as $passenger) {
+        if (preg_match("/$name/i", $passenger['Name'])) {
+            $results[] = $passenger;
+        }
+    }
+    return $results;
+}
+
+// Получение параметров запроса
+$age = isset($_GET['age']) ? $_GET['age'] : '';
+$name = isset($_GET['name']) ? $_GET['name'] : '';
+
+// Выполнение поиска
+if (!empty($age)) {
+    $results = searchByAge($passengers, $age);
+} elseif (!empty($name)) {
+    $results = searchByName($passengers, $name);
 } else {
-    
-    echo "Ошибка";}
+    $results = $passengers;
+}
+
+// Создание HTML-страницы с результатами
+$html = '<html>
+<head>
+    <title>Результаты поиска пассажиров</title>
+</head>
+<body>
+    <h1>Результаты поиска пассажиров</h1>
+    <table>
+        <tr>
+            <th>Имя</th>
+            <th>Возраст</th>
+            <th>Пол</th>
+            <th>Класс</th>
+            <th>Выжил</th>
+        </tr>';
+foreach ($results as $result) {
+    $html .= '<tr>
+                <td>'.$result['Name'].'</td>
+                <td>'.$result['Age'].'</td>
+                <td>'.$result['Sex'].'</td>
+                <td>'.$result['Pclass'].'</td>
+                <td>'.$result['Survived'].'</td>
+            </tr>';
+}
+$html .= '</table>
+</body>
+</html>';
+
+// Вывод HTML-страницы
+echo $html;
 ?>
